7d0ca5bf33529ae9c756fff6e42f673098f4d9af
David Anderson <davidandersonsix@gmail.com>
2024-02-25 20:54:51 -0500

    Updated styles

--- table-topics/src/components/Button.js
+++ table-topics/src/components/Button.js
@@ -5,7 +5,7 @@ export default function Button({ className, text, onClick }) {
   return (
     <button
       onClick={onClick}
-      className={`bg-teal-700 text-slate-200 px-5 py-1 rounded-full 
+      className={`text-slate-200 px-5 py-1 rounded-full 
       text-2xl font-semibold appearance-none ${style}
       transition-all duration-300`}
     >
