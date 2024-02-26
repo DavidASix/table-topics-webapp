d48f5f74d7fd70ce2db63395722aa7c8c1780b82
David Anderson <davidandersonsix@gmail.com>
2024-02-25 21:44:43 -0500

    Updated styles, added buton disable prop

--- table-topics/src/components/Button.js
+++ table-topics/src/components/Button.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-export default function Button({ className, text, onClick }) {
+export default function Button({ disabled, className, text, onClick }) {
   const style = className ? className : "";
   return (
     <button
+      disabled={disabled}
       onClick={onClick}
       className={`text-slate-200 px-5 py-1 rounded-full 
       text-2xl font-semibold appearance-none ${style}
